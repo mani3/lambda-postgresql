69022a73c090d5dbfd69fb8bde7cbae961656628
mani3 <kazuya.4da@gmail.com>
2024-02-12 21:18:53 +0900

    :recycle: refactor for ruff

--- src/handler.py
+++ src/handler.py
@@ -4,7 +4,7 @@ from datetime import timedelta, timezone
 
 import psycopg2
 
-JST = timezone(timedelta(hours=+9), 'JST')
+JST = timezone(timedelta(hours=+9), "JST")
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -16,7 +16,7 @@ def execute_sql(database_url, sql):
   with psycopg2.connect(database_url) as conn:
     with conn.cursor() as cursor:
       cursor.execute(sql)
-      if 'SELECT' in cursor.statusmessage:
+      if "SELECT" in cursor.statusmessage:
         rows = cursor.fetchall()
   return rows
 
@@ -39,10 +39,10 @@ def column_all_sql():
 def main(event, context):
   logger.info(event)
 
-  database_url = os.environ.get('DATABASE_URL')
+  database_url = os.environ.get("DATABASE_URL")
 
   if database_url is None:
-    raise Exception('Not found DATABASE_URL environment value')
+    raise Exception("Not found DATABASE_URL environment value")
 
   rows = execute_sql(database_url, column_all_sql())
   logger.info(rows)
